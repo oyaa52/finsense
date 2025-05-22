56cd9fcf0ce0d5bf48868da93219de56a08db5b8
yeonjilim <imyeonji520@gmail.com>
2025-05-23 00:10:13 +0900

    [BE] accounts User serializer 작성

--- final_pjt_back/accounts/serializers.py
+++ final_pjt_back/accounts/serializers.py
@@ -1,16 +1,22 @@
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from rest_framework import serializers
+from products.models import DepositProduct  # 실제 모델 import 필요
 
 class CustomRegisterSerializer(RegisterSerializer):
-    subscribed_products = serializers.CharField()
+    subscribed_products = serializers.PrimaryKeyRelatedField(
+        queryset=DepositProduct.objects.all(),
+        many=True,
+        required=False
+    )
 
     def get_cleaned_data(self):
-        data =  super().get_cleaned_data()
-        data['subscribed_products'] = self.validated_data.get('subscribed_products')
+        data = super().get_cleaned_data()
+        data['subscribed_products'] = self.validated_data.get('subscribed_products', [])
         return data
-    
+
     def save(self, request):
         user = super().save(request)
-        user.subscribed_products = self.validated_data.get('subscribed_products')
+        subscribed_products = self.validated_data.get('subscribed_products', [])
         user.save()
+        user.subscribed_products.set(subscribed_products)  # ManyToManyField 관계 설정
         return user
