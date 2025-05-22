614ff46a8a8394625e8cfa62322508b7bdbbddcf
LEESEOKJAE <lsj6924@naver.com>
2025-05-22 16:55:58 +0900

    [FE] feat : render 페이지 aos init 수정

--- final_pjt_front/src/App.vue
+++ final_pjt_front/src/App.vue
@@ -1,23 +1,32 @@
 <template>
-    <h1>Hello!</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est earum perspiciatis ullam tempora voluptas quis aliquid quisquam! Ipsa corrupti, unde nam perspiciatis impedit vero eligendi assumenda doloribus rerum blanditiis maxime!</p>
-    <p v-for="i in 30" :key="i">Dummy content {{ i }}</p>
-    <div data-aos="fade-in">
-        <img src="@/assets/travel.png" alt="travel">
-    </div>
+  <h1>Hello!</h1>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est earum perspiciatis ullam tempora voluptas quis
+    aliquid quisquam! Ipsa corrupti, unde nam perspiciatis impedit vero eligendi assumenda doloribus rerum blanditiis
+    maxime!</p>
+  <p v-for="i in 30" :key="i">Dummy content {{ i }}</p>
+  <div data-aos="fade-in" data-aos-once="false">
+    <img src="@/assets/travel.png" alt="travel" style="max-width: 100%; height: auto;">
+  </div>
 
 </template>
 
 <script setup>
-// <script setup>
 import { onMounted } from 'vue'
 import AOS from 'aos'
+import 'aos/dist/aos.css'  // AOS 스타일시트 import
 
 onMounted(() => {
-  AOS.refresh() // 새로 로드된 요소에 적용
+  AOS.init({
+    duration: 3000,
+    once: true
+  })
+  AOS.refresh()
 })
 </script>
 
 <style scoped>
-
+img {
+  display: block;
+  margin: 20px auto;
+}
 </style>
