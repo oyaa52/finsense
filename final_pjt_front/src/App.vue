26fe03fefa7ab174548f6d000e9080395805f61d
yeonji <imyeonji520@gmail.com>
2025-05-22 16:26:50 +0900

    랜딩페이지 스크롤 인터렉션 기능 구현 중

--- final_pjt_front/src/App.vue
+++ final_pjt_front/src/App.vue
@@ -1,9 +1,21 @@
 <template>
+    <h1>Hello!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est earum perspiciatis ullam tempora voluptas quis aliquid quisquam! Ipsa corrupti, unde nam perspiciatis impedit vero eligendi assumenda doloribus rerum blanditiis maxime!</p>
+    <p v-for="i in 30" :key="i">Dummy content {{ i }}</p>
+    <div data-aos="fade-in">
+        <img src="@/assets/travel.png" alt="travel">
+    </div>
 
 </template>
 
 <script setup>
+// <script setup>
+import { onMounted } from 'vue'
+import AOS from 'aos'
 
+onMounted(() => {
+  AOS.refresh() // 새로 로드된 요소에 적용
+})
 </script>
 
 <style scoped>
