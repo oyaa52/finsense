9b148e736556fbfaedc2180ac5db33ab1c598468
LEESEOKJAE <lsj6924@naver.com>
2025-05-23 12:23:47 +0900

    [BE] feat: 예적금 상품 모델링 분리 및 추상 클래스 도입, 조회 API 구현

--- final_pjt_back/backend/urls.py
+++ final_pjt_back/backend/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('api/v1/products/', include('products.urls')),
+    path("admin/", admin.site.urls),
+    path("dj-rest-auth/", include("dj_rest_auth.urls")),
+    path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("api/v1/products/", include("products.urls")),
 ]
