e85af138dd585c62c9c67b3012b71cf4b5f29bed
yeonji <imyeonji520@gmail.com>
2025-05-23 10:34:28 +0900

    [BE] style : products의  urls와 views에 작성한 주석 없어진 것 같아 추가함

--- final_pjt_back/products/urls.py
+++ final_pjt_back/products/urls.py
@@ -5,10 +5,9 @@ app_name = "products"
 
 urlpatterns = [
     # 예금 상품 정보 저장
-    path("save-deposit-products/", views.save_deposit_products, name="save_deposit_products"),
-    # path("deposit-products/<str:fin_prdt_cd>/", views.deposit_product_options, name="deposit_product_options"),
-    # path("deposit-products/top_rate/", views.top_rate, name="top_rate"),
-    # path("deposit-products/", views.deposit_products, name="deposit_products"),
+    path('save_deposit_products/', views.save_deposit_products),
+    # path('deposit_product_options/<str:fin_prdt_cd>/', views.deposit_product_options),
+    # path('deposit-products/top-rate/', views.top_rate),
+    # path('deposit-products/', views.deposit_products)
     # views.py에 있는 함수들에 대한 경로, 추후 필요할 경우 사용 예정(05/23)
-    path("get-kakao-map-api-key/", views.get_kakao_map_api_key, name="get_kakao_map_api_key"),
 ]
