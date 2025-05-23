1441db02ef031ea25e60077f9a40b11f6b444e37
LEESEOKJAE <lsj6924@naver.com>
2025-05-23 16:01:06 +0900

    [BE] feat(products & assetinfo): 상품 구독 및 구독 목록 조회 API 구현,현물 가격 데이터 처리 및 API구현

--- final_pjt_back/assetinfo/urls.py
+++ final_pjt_back/assetinfo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views  # assetinfo 앱 내의 views.py를 임포트합니다.
+from .views import AssetPriceListAPIView  # AssetPriceListAPIView 임포트
 
-app_name = 'assetinfo'
+app_name = "assetinfo"
 
 urlpatterns = [
     # 현물 데이터 제공 API
-    path('chart-data/', views.asset_prices_for_chart, name='asset_prices_for_chart'),
+    path("chart-data/", views.asset_prices_for_chart, name="asset_prices_for_chart"),
+    path("prices/", AssetPriceListAPIView.as_view(), name="asset-price-list"),
 ]
