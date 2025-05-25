3bd6e9613a57153a718b8f77d854fb590503fa7c
LEESEOKJAE <lsj6924@naver.com>
2025-05-25 13:09:17 +0900

    [FE, BE] feat(프로필이미지 기능 구현 및 UI 개선)

--- final_pjt_back/backend/urls.py
+++ final_pjt_back/backend/urls.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings # 설정 파일을 가져오기 위해 추가
+from django.conf.urls.static import static # static 파일 관련 함수 추가
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("dj-rest-auth/", include("dj_rest_auth.urls")),
     path("dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
     path("api/v1/accounts/", include("accounts.urls")),
+    # path("api/v1/community/", include("community.urls")), # community 앱에 urls.py 부재로 임시 주석 처리
     path("api/v1/products/", include("products.urls")),
-    path("api/v1/kakaomap/", include("kakaomap.urls")),
-    path("api/v1/assetinfo/", include("assetinfo.urls")),
     path("api/v1/recommendations/", include("recommendations.urls")),
+    path("api/v1/assetinfo/", include("assetinfo.urls")),
+    path("api/v1/kakaomap/", include("kakaomap.urls")),
 ]
+
+# 개발 환경에서 미디어 파일을 서빙하기 위한 설정 추가
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
